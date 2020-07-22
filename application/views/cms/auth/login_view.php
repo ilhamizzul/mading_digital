6d4d36b63c7521647c4423b95e1058b5fc280466
Ilham Izzul Hadyan <ilhamizzul@gmail.com>
2020-07-22 11:24:52 +0700

    enhance login view

--- application/views/cms/auth/login_view.php
+++ application/views/cms/auth/login_view.php
@@ -48,10 +48,10 @@
       }
     ?>
 		<!-- Outer Row -->
-		<div class="row justify-content-center">
+		<div class="row justify-content-center mt-5 pt-lg-5">
 			<div class="col-xl-10 col-lg-12 col-md-9">
-				<div class="card o-hidden border-0 shadow-lg my-5">
-					<div class="card-body p-0">
+				<div class="card o-hidden border-0 shadow-lg">
+					<div class="card-body p-lg-5 p-0">
 						<!-- Nested Row within Card Body -->
 						<div class="row">
 							<div class="col-lg-6 d-none d-lg-block bg-login-image"></div>
