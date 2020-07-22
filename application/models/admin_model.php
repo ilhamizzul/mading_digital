5161e1a9a47d4862b262b1a5f59928fa367c301e
Ilham Izzul Hadyan <ilhamizzul@gmail.com>
2020-07-22 14:37:40 +0700

    initial view configuration for user management

--- application/models/admin_model.php
+++ application/models/admin_model.php
@@ -4,7 +4,11 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class admin_model extends CI_Model {
 
-    
+    public function get_all_users()
+    {
+        return $this->db->get_where('tb_user', ['id_company' => $this->session->userdata('id_company')])
+                        ->result_array();
+    }
 
 }
 
