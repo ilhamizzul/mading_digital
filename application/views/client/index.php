c426aaa05a09366739a4da44610768984a408082
Ilham Izzul Hadyan <ilhamizzul@gmail.com>
2020-07-27 15:17:39 +0700

    add meta refresh page

--- application/views/client/index.php
+++ application/views/client/index.php
@@ -4,6 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="refresh" content="360">
     <title><?= $title; ?></title>
     <link rel="stylesheet" href="<?= base_url() ?>assets/client/vendor/bootstrap/css/bootstrap.min.css">
     <link rel="stylesheet" href="<?= base_url() ?>assets/client/css/style.css">
