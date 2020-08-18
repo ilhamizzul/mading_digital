d29613d7e784faeafd26f81587d50625500aa182
Ilham Izzul Hadyan <ilhamizzul@gmail.com>
2020-08-18 14:55:15 +0700

    add 403 page

---
+++ application/views/403_view.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<meta name="description" content="">
+	<meta name="author" content="">
+
+	<title>403 Forbidden</title>
+
+	<!-- Custom fonts for this template-->
+	<link href="<?= base_url() ?>assets/cms/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+	<link
+		href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
+		rel="stylesheet">
+
+	<!-- Custom styles for this template-->
+	<link href="<?= base_url() ?>assets/cms/css/sb-admin-2.min.css" rel="stylesheet">
+
+</head>
+
+<body class="bg-gradient-primary">
+
+	<div class="container">
+		<div class="row justify-content-center mt-5 pt-lg-5">
+			<div class="col-xl-10 col-lg-12 col-md-9">
+				<div class="card o-hidden border-0 shadow-lg">
+					<div class="card-body p-lg-5 p-0">
+						<h1 class="text-center text-primary display-1">403</h1>
+						<h1 class="text-center mt-1 font-weight-bold text-primary">Hold Up!</h1>
+                        <h2 class="text-center text-primary">That page is forbidden!</h2>
+                        <h5 class="text-center mt-3 w-75 mx-auto d-block text-default">You'll probably need to purchase your account if you're gonna get those kinds of privelege</h5>
+						<a href="<?= base_url("Dashboard") ?>" class="btn btn-lg btn-default text-primary font-weight-bold mx-auto d-block" ><span class="fa fa-arrow-left"></span> Get Back</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<!-- Bootstrap core JavaScript-->
+	<script src="<?= base_url() ?>assets/cms/vendor/jquery/jquery.min.js"></script>
+	<script src="<?= base_url() ?>assets/cms/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+	<!-- Core plugin JavaScript-->
+	<script src="<?= base_url() ?>assets/cms/vendor/jquery-easing/jquery.easing.min.js"></script>
+
+	<!-- Custom scripts for all pages-->
+	<script src="<?= base_url() ?>assets/cms/js/sb-admin-2.min.js"></script>
+
+</body>
+
+</html>
