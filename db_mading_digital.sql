0b39168d9c84e53b97143eea3033a410e682c1f3
Ilham Izzul Hadyan <ilhamizzul@gmail.com>
2020-08-19 13:17:51 +0700

    change db

--- db_mading_digital.sql
+++ db_mading_digital.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 11, 2020 at 11:26 AM
+-- Generation Time: Aug 19, 2020 at 08:17 AM
 -- Server version: 10.4.11-MariaDB
 -- PHP Version: 7.4.2
 
@@ -42,7 +42,7 @@ CREATE TABLE `tb_admin` (
 --
 
 INSERT INTO `tb_admin` (`id_admin`, `user_name`, `username`, `password`, `profile_picture`, `email`) VALUES
-(1, 'Ilham Izzul Hadyan', 'ilhamizzul', '$2y$10$LOacn0VbbFC4Ty8gv5Jg9eEnLOKgDPxiYJAefgkxcQTJb98xyFLtu', '', 'ilhamizzul@gmail.com');
+(1, 'Ilham Izzul Hadyan', 'ilhamizzul', '$2y$10$JohXok5A7XIoaf3t3edsgOksQxscWlzxiRlkkNZS3TLo626r07wVu', '', 'ilhamizzul@gmail.com');
 
 -- --------------------------------------------------------
 
@@ -57,7 +57,7 @@ CREATE TABLE `tb_carousel` (
   `description` varchar(100) NOT NULL,
   `data_type` enum('image','video') NOT NULL,
   `active` enum('false','true') NOT NULL,
-  `id_company` varchar(15) NOT NULL,
+  `id_company` varchar(40) NOT NULL,
   `id_repeater` varchar(10) NOT NULL,
   `createdAt` datetime NOT NULL,
   `activedAt` datetime NOT NULL
@@ -80,7 +80,7 @@ INSERT INTO `tb_carousel` (`id_carousel`, `data_carousel`, `title`, `description
 
 CREATE TABLE `tb_client_coloring` (
   `id_color` varchar(15) NOT NULL,
-  `id_company` varchar(15) NOT NULL,
+  `id_company` varchar(40) NOT NULL,
   `title` varchar(25) NOT NULL,
   `bg_color1` varchar(7) NOT NULL,
   `bg_color2` varchar(7) NOT NULL,
@@ -98,7 +98,7 @@ CREATE TABLE `tb_client_coloring` (
 INSERT INTO `tb_client_coloring` (`id_color`, `id_company`, `title`, `bg_color1`, `bg_color2`, `bg_color3`, `nav_color`, `txt_color`, `txt_news_color`, `active_color`) VALUES
 ('COL-20080400001', 'COM1000001', 'vnfdks', '#db2929', '#ba0303', '#642b2b', '#ba1212', '#f46767', '#240000', 0),
 ('COL-20080400002', 'COM1000001', 'ufbes', '#1aff47', '#1916e9', '#e63333', '#db2424', '#15a8b2', '#f5f5f5', 1),
-('COL-20081100001', 'COM-20081100001', 'Default', '#023e8a', '#0096c7', '#48cae4', '#03045e', 'white', '#48cae4', 1),
+('COL-20081900001', 'GvU4oVGj1NonG30otqMhfmw2PfsqWTYbesisEOAs', 'Default', '#023e8a', '#0096c7', '#48cae4', '#03045e', 'white', '#48cae4', 1),
 ('COL200804000001', 'COM1000001', 'Default', '#023e8a', '#0096c7', '#48cae4', '#03045e', 'white', '#48cae4', 0);
 
 -- --------------------------------------------------------
@@ -108,7 +108,7 @@ INSERT INTO `tb_client_coloring` (`id_color`, `id_company`, `title`, `bg_color1`
 --
 
 CREATE TABLE `tb_company` (
-  `id_company` varchar(15) NOT NULL,
+  `id_company` varchar(40) NOT NULL,
   `company_name` varchar(50) NOT NULL,
   `company_logo` varchar(20) NOT NULL,
   `email` varchar(70) NOT NULL,
@@ -125,8 +125,8 @@ CREATE TABLE `tb_company` (
 --
 
 INSERT INTO `tb_company` (`id_company`, `company_name`, `company_logo`, `email`, `address`, `activeStatus`, `firstLogin`, `onTrial`, `validity`, `createdAt`) VALUES
-('COM-20081100001', 'tani makmur 2', '', 'kukuhwidiatmoko0@gmail.com', '', 1, 0, 1, '2020-08-15', '2020-08-11 06:38:43'),
-('COM1000001', 'Pyxis', 'logo.png', 'johnDoe@gmail.com', 'malang', 1, 0, 0, '2020-09-30', '2020-07-28 13:45:24');
+('COM1000001', 'Pyxis', 'logo.png', 'ilhamizzul@student.telkomuniversity.ac.id', 'malang', 1, 0, 0, '2020-08-31', '2020-07-28 13:45:24'),
+('GvU4oVGj1NonG30otqMhfmw2PfsqWTYbesisEOAs', 'Tani Makmur', '', 'ilham_hadyan_24rpl@student.smktelkom-mlg.sch.id', '', 1, 0, 0, '2022-02-19', '2020-08-19 04:50:39');
 
 -- --------------------------------------------------------
 
@@ -136,7 +136,7 @@ INSERT INTO `tb_company` (`id_company`, `company_name`, `company_logo`, `email`,
 
 CREATE TABLE `tb_content_grp` (
   `id` int(11) NOT NULL,
-  `id_company` varchar(15) NOT NULL,
+  `id_company` varchar(40) NOT NULL,
   `description` varchar(50) NOT NULL,
   `active` enum('false','true') NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
@@ -149,11 +149,11 @@ INSERT INTO `tb_content_grp` (`id`, `id_company`, `description`, `active`) VALUE
 (1, 'COM1000001', 'footer', 'true'),
 (2, 'COM1000001', 'carousel', 'true'),
 (3, 'COM1000001', 'schedule', 'true'),
-(4, 'COM1000001', 'navigation_bar', 'false'),
-(5, 'COM-20081100001', 'footer', 'true'),
-(6, 'COM-20081100001', 'carousel', 'true'),
-(7, 'COM-20081100001', 'schedule', 'true'),
-(8, 'COM-20081100001', 'navigation_bar', 'true');
+(4, 'COM1000001', 'navigation_bar', 'true'),
+(17, 'GvU4oVGj1NonG30otqMhfmw2PfsqWTYbesisEOAs', 'footer', 'true'),
+(18, 'GvU4oVGj1NonG30otqMhfmw2PfsqWTYbesisEOAs', 'carousel', 'true'),
+(19, 'GvU4oVGj1NonG30otqMhfmw2PfsqWTYbesisEOAs', 'schedule', 'true'),
+(20, 'GvU4oVGj1NonG30otqMhfmw2PfsqWTYbesisEOAs', 'navigation_bar', 'true');
 
 -- --------------------------------------------------------
 
@@ -169,7 +169,7 @@ CREATE TABLE `tb_info` (
   `info_type` enum('event','slogan','news') NOT NULL,
   `active` enum('false','true') NOT NULL,
   `id_repeater` varchar(10) NOT NULL,
-  `id_company` varchar(15) NOT NULL,
+  `id_company` varchar(40) NOT NULL,
   `createdAt` datetime NOT NULL,
   `activedAt` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
@@ -212,6 +212,56 @@ INSERT INTO `tb_repeater` (`id_repeater`, `description`, `delete_status`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `tb_token`
+--
+
+CREATE TABLE `tb_token` (
+  `token` varchar(30) NOT NULL,
+  `token_type` enum('1month','3months','1year') NOT NULL,
+  `active` tinyint(1) NOT NULL,
+  `send_status` tinyint(1) NOT NULL,
+  `createdAt` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tb_token`
+--
+
+INSERT INTO `tb_token` (`token`, `token_type`, `active`, `send_status`, `createdAt`) VALUES
+('3lVtAVuK87ALmJBvafynqaytDWT9Z5', '1year', 1, 1, '2020-08-19 04:18:05'),
+('3mfwD8AW0bhg0o4nIl8dNlyEjXIGOi', '1year', 0, 0, '2020-08-19 04:18:05'),
+('62MPdHYBkvb8E9rREOEdBA4qCRNjFP', '3months', 0, 1, '2020-08-19 05:02:26'),
+('81Nv1xfA2LRpLOfkuUOzEhe9mMHY8J', '1month', 0, 1, '2020-08-19 05:03:50'),
+('92AbIiQMLoZn8nZNhRgOhoxc9wal5i', '3months', 1, 1, '2020-08-19 05:02:26'),
+('cCsr9MomTQGexzTgkg7BHiKp8kd4gR', '1month', 1, 1, '2020-08-19 05:03:50'),
+('CXH2h45EGiazymHxR5AM0QpXvb0R09', '3months', 0, 0, '2020-08-19 05:02:26'),
+('EDQM04RYVQflWrE7SvFLtILU9fDQpr', '1month', 1, 1, '2020-08-19 05:03:50'),
+('edUTYVwgYjksMQFpjOP5CYGxzi2pnW', '3months', 0, 0, '2020-08-19 05:02:26'),
+('EPqnERUdQEgFnVnw81WcRHH0oYqlAh', '1year', 0, 0, '2020-08-19 04:18:05'),
+('FfEH5mGp0G7TXF7mgFNaJA90w7WquL', '3months', 0, 0, '2020-08-19 05:02:26'),
+('GDJCK9x7foMzK6NbLCCi07ZmXzcJJI', '3months', 0, 0, '2020-08-19 05:02:26'),
+('GJfHIaX4FpnVloLzs4yNHzIIX7sewN', '1month', 0, 0, '2020-08-19 05:03:50'),
+('hBuHLdxNrVCquWzyus3O9sv4NrLxE5', '1month', 0, 0, '2020-08-19 05:03:50'),
+('HZwKUwLNRQa7jduRlzu2KKPlq7mG2u', '1month', 0, 0, '2020-08-19 05:03:50'),
+('iIN7lW3O8oe7mRsj6FYwoTdidEDwcI', '1month', 0, 0, '2020-08-19 05:03:50'),
+('JLzCTGjte5wX0f8nL1lwbJcezAomnX', '1year', 0, 0, '2020-08-19 04:18:05'),
+('L2WQBUm3fhjTvhujMbHirfPMhtpiu3', '1month', 0, 0, '2020-08-19 05:03:50'),
+('L87vwVodSXD0axApRY32s8tj5dksVM', '1month', 0, 0, '2020-08-19 05:03:50'),
+('mQ8lNCfCB1iXVQR4roUPdjz9Mr8tFc', '3months', 0, 0, '2020-08-19 05:02:26'),
+('Mt5qZuyrzu04BvWvllLP8Pbom3CGQj', '1year', 0, 0, '2020-08-19 04:18:05'),
+('nQPieuK9R7adNbyS4cPiEZNxSUSHSk', '1year', 0, 0, '2020-08-19 04:18:05'),
+('PayjNBARg04EPvi5ZmM9aIb4z5RWRt', '1month', 0, 0, '2020-08-19 05:03:50'),
+('PDTdlzhBnJIrStGMXnIvgPGeOBsyaK', '3months', 0, 0, '2020-08-19 05:02:26'),
+('r9dIi95fBc1pdsv9fLTab0NllnyEU4', '1year', 0, 0, '2020-08-19 04:18:05'),
+('uH8P7SXtxrc3VhS41KJ4QmWgQut3ph', '3months', 0, 0, '2020-08-19 05:02:26'),
+('vJ0j4CnZlmzb7HrnQyNSsGG3MmNVcg', '1year', 0, 0, '2020-08-19 04:18:05'),
+('ygxdOxxzL9bQJym6fa5wNZWU6kA2G7', '1year', 0, 0, '2020-08-19 04:18:05'),
+('zcYHDyMUjOM3zqxkYzkD0zwtUjHVDt', '3months', 0, 0, '2020-08-19 05:02:26'),
+('zlbf84CKRHZ7Ol1pfKKY5vIhajVIOz', '1year', 0, 0, '2020-08-19 04:18:05');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `tb_user`
 --
 
@@ -222,8 +272,8 @@ CREATE TABLE `tb_user` (
   `password` varchar(60) NOT NULL,
   `profile_picture` varchar(25) NOT NULL,
   `address` varchar(250) NOT NULL,
-  `email` varchar(35) NOT NULL,
-  `id_company` varchar(15) NOT NULL,
+  `email` varchar(70) NOT NULL,
+  `id_company` varchar(40) NOT NULL,
   `active` enum('false','true') NOT NULL,
   `role` enum('owner','admin') NOT NULL,
   `createdAt` datetime NOT NULL
@@ -238,7 +288,7 @@ INSERT INTO `tb_user` (`id_user`, `user_name`, `username`, `password`, `profile_
 ('US-200723000002', 'Ilham Izzul Hadyan', 'ilhamizzul', '$2y$10$wSnL2uw.ycuyh0Ufpx1I6.AoCBVA69EdbD.BfJyArpmuTUcGWM9L6', 'profile.jpg', 'malang', 'ilhamizzul@gmail.com', 'COM1000001', 'true', 'owner', '2020-08-03 09:13:44'),
 ('US-200728000001', 'admin', 'admin', '$2y$10$WqiuQOegd7phRdYClgVgHORguniPIgSZfNOcgHpzxK8MscHPCaL9O', '', '', '', 'COM1000001', 'true', 'admin', '2020-07-30 08:37:28'),
 ('US-200728000002', 'owner', 'owner', '$2y$10$JZkw4jq1uyzL8U2z0gj5POL9WoZhEF/ybctt5VFytX8xMV3d8FHMa', '', '', '', 'COM1000001', 'true', 'owner', '2020-07-28 15:53:11'),
-('US-200811000001', 'kukuh widiatmoko', 'owner2', '$2y$10$cM3nMZt4UsqXwxo.p1OaieSEjh2MnnFFToSCQY56Q8E13MNQQBXRu', '', '', 'kukuhwidiatmoko0@gmail.com', 'COM-20081100001', 'true', 'owner', '2020-08-11 11:41:42');
+('US-200819000001', 'Reza Faisal M', 'rezafaisal', '$2y$10$enFc9GNjyfT9PDRNFoux2uuolGPbXaeS2RUeUS.41myLAhS6135Z2', '', '', 'ilhamizzul@student.telkomuniversity.ac.id', 'GvU4oVGj1NonG30otqMhfmw2PfsqWTYbesisEOAs', 'true', 'owner', '2020-08-19 04:50:39');
 
 --
 -- Indexes for dumped tables
@@ -270,7 +320,8 @@ ALTER TABLE `tb_client_coloring`
 --
 ALTER TABLE `tb_company`
   ADD PRIMARY KEY (`id_company`),
-  ADD UNIQUE KEY `email` (`email`);
+  ADD UNIQUE KEY `email` (`email`),
+  ADD UNIQUE KEY `company_name` (`company_name`);
 
 --
 -- Indexes for table `tb_content_grp`
@@ -292,6 +343,12 @@ ALTER TABLE `tb_info`
 --
 ALTER TABLE `tb_repeater`
   ADD PRIMARY KEY (`id_repeater`);
+
+--
+-- Indexes for table `tb_token`
+--
+ALTER TABLE `tb_token`
+  ADD PRIMARY KEY (`token`);
 
 --
 -- Indexes for table `tb_user`
@@ -316,7 +373,7 @@ ALTER TABLE `tb_admin`
 -- AUTO_INCREMENT for table `tb_content_grp`
 --
 ALTER TABLE `tb_content_grp`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
 
 --
 -- Constraints for dumped tables
