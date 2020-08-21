7f094a26e17f45550e521aa68e04ac82158d096b
Ilham Izzul Hadyan <ilhamizzul@gmail.com>
2020-08-21 13:57:06 +0700

    delete unnecesary function

--- application/controllers/Company.php
+++ application/controllers/Company.php
@@ -21,25 +21,6 @@ class Company extends CI_Controller {
         }
     }
 
-    // private function deleteDir($dirPath)
-    // {
-    //     if (! is_dir($dirPath)) {
-    //         throw new InvalidArgumentException("$dirPath must be a directory");
-    //     }
-    //     if (substr($dirPath, strlen($dirPath) - 1, 1) != '/') {
-    //         $dirPath .= '/';
-    //     }
-    //     $files = glob($dirPath . '*', GLOB_MARK);
-    //     foreach ($files as $file) {
-    //         if (is_dir($file)) {
-    //             self::deleteDir($file);
-    //         } else {
-    //             unlink($file);
-    //         }
-    //     }
-    //     rmdir($dirPath);
-    // }
-
     private function _is_superadmin()
     {
         if ($this->session->userdata('role') == 'superadmin') {
